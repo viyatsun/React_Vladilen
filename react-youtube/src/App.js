8a853c64179ca4588273c123d71a00959780cf75
Ksyunomorph <49567382+viyatsun@users.noreply.github.com>
2020-09-09 00:10:04 -0200

    useState

---
+++ react-youtube/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TodoList from './todo/todo-list';
+import { func } from 'prop-types';
+
+
+export default function App() {
+  const [todos, setTodos] = React.useState([
+    {id:1,completed: false, title: 'Придбати тачку'},
+    {id:2,completed: false, title: 'Придбати хату'},
+    {id:3,completed: false, title: 'Придбати комп'}
+  ])
+
+  function toggleTodo (id) {
+    setTodos(
+      todos.map(todo=> {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
+  }
+
+  return (
+    <div className='wrapper'>
+      <h1>React tutorial</h1>
+
+      <TodoList todos={todos} onToggle={toggleTodo}/>
+    </div>
+  )
+}
+
---
+++ react-youtube/src/todo/todo-item.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const styles = {
+    li: {
+        display:'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding:'.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginButtom: '.5rem',
+        margin: '1rem'
+    },
+    input: {
+        marginRight: '1rem'
+    }
+}
+
+function TodoItem ({ todo, index, onChange }) {
+    console.log('todo',todo)
+    return (
+    <li style={styles.li}>
+        <span>
+            <input type = "checkbox" style={styles.input} onChange={()=>onChange(todo.id)}/>
+            <strong>{index+1}</strong>
+            &nbsp;
+            {todo.title}
+        </span>
+
+        <button className="rm">&times;</button>
+        
+    </li>
+    )
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired,
+    index: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+}
+
+export default TodoItem;
+
